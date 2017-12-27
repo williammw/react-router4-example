cbf8f0762d2a908a63f51addce1db297dfbee14e
Man Wai Wong <william.manwai@gmail.com>
2017-12-27 23:37:04 +0800

    react-router added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route, Link, NavLink} from 'react-router-dom';
 
+const isActiveFunc = (match, location) => {
+  console.log(match, location)
+  return match;
+}
+
 const Links = () => (
   <nav>
     <NavLink exact activeClassName="active" to="/">Home</NavLink>
-    <NavLink activeClassName="active" to={{ pathname: "/index.aspx/about" }}>About</NavLink>
-    <NavLink replace to="/contact">Contact</NavLink>
+    <NavLink activeStyle={{color:'green'}} to={{ pathname: "/index.aspx/about" }}>About</NavLink>
+    <NavLink 
+    isActive={isActiveFunc}
+    activeClassName="active"
+     to="/contact">Contact</NavLink>
   </nav>
 )
 
@@ -17,8 +25,24 @@ const App = (props) => (
       <Route exact path="/" render={() => <h1>Home</h1>} />
       <Route path="/index.aspx/about" render={() => <h1>About</h1>} />
       <Route path="/contact" render={() => <h1>Contact</h1>} />
+      <Route path="/:page?-:subpage" render={({match}) => (
+        <h1>
+          PAGE: {match.params.page || 'GGome'}<br />
+          SUBPAGE: {match.params.subpage}
+        </h1>
+      )} />
     </div>
   </Router>
 );
+// step 1
+// <Route path="/:page" render={({match}) => (
+//   <h1>
+//     PAGE: {match.params.page || 'GGome'}
+//   </h1>
+// )} />
 
+// important
+// <Route path="/:page?/:subpage" 
+// <Route path="/:page?-:subpage" 
+// 
 export default App;
