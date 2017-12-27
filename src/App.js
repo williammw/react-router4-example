51d5f213336a893b9b79eafd5d426adf013c27fb
Man Wai Wong <william.manwai@gmail.com>
2017-12-27 23:17:06 +0800

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
+const Home = (props) => <h1>HOME</h1>
 
 class App extends Component {
   render() {
     return (
-     <div>Hello world</div>
+     <Router>
+     <div>
+        <Route exact path="/" component={Home} />
+        {/*<Route path="/about" render={() => <h1>About</h1>} />*/}
+        <Route path="/about" children={({match}) => match &&  <h1>About</h1>} />
+      </div>
+    </Router>
+     
     );
   }
 }
